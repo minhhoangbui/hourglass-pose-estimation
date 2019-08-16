d1fe60222e70d8bc9a520c62f1accaa4da75cdf6
minhhoang <hoang.bui@awl.com.vn>
2019-08-16 10:15:21 +0700

    merl3k son

--- pose/datasets/__init__.py
+++ pose/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .lsp import lsp
 from .mpii import mpii
 from .mscoco import mscoco
+from .merl3k import merl3k
 
-__all__ = ('mpii', 'lsp', 'mscoco')
+__all__ = ('mpii', 'lsp', 'mscoco', 'merl3k')

cc7552aa305bbeea589c162a11a3d117472850c7
minhhoang <hoang.bui@awl.com.vn>
2019-09-30 11:11:39 +0700

    before merge

--- pose/datasets/__init__.py
+++ pose/datasets/__init__.py
@@ -4,5 +4,6 @@ from .mscoco import mscoco
 from .mpii_v2 import mpii_v2
 from .mscoco_v2 import mscoco_v2
 from .merl3k import merl3k
+from .crowdpose import crowdpose
 
-__all__ = ('mpii', 'lsp', 'mscoco', 'mpii_v2', 'mscoco_v2', 'merl3k')
+__all__ = ('mpii', 'lsp', 'mscoco', 'mpii_v2', 'mscoco_v2', 'merl3k', 'crowdpose')
